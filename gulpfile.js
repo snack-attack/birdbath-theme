329ea6feb0e8d742e94fd621065211e4239b5553
Audrey Hayes <audreyhayes0@gmail.com>
2019-02-17 10:55:49 +0000

    added first gulp task - compile scss to css

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,12 @@
-‘use strict’;
+let gulp = require('gulp');
+let cleanCSS = require('gulp-clean-css');
+let rename = require('gulp-rename');
+let sass = require('gulp-sass');
 
-var gulp = require(‘gulp’);
+gulp.task('sass', function () {
+    var stream = gulp.src('./scss/styles.scss')
+        .pipe(sass())
+        .pipe(gulp.dest('./css/'))
+        .pipe(rename('styles.css'));
+    return stream;
+});
