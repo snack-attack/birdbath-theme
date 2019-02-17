5b483a2501c19f2121f31873bcd10b6240601cae
Audrey Hayes <audreyhayes0@gmail.com>
2019-02-17 11:59:20 +0000

    adding gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -2,11 +2,18 @@ let gulp = require('gulp');
 let cleanCSS = require('gulp-clean-css');
 let rename = require('gulp-rename');
 let sass = require('gulp-sass');
+let uglify = require('gulp-uglify-es');
+let autoprefix = require('gulp-autoprefixer');
+let concat = require('gulp-concat');
+let imagemin = require('gulp-imagemin');
+let browserSync = require('browser-sync').create();
+
+//compile sass and minify css
 
 gulp.task('sass', function () {
     var stream = gulp.src('./scss/styles.scss')
         .pipe(sass())
-        .pipe(gulp.dest('./css/'))
+        .pipe(gulp.dest('./dist/css/'))
         .pipe(rename('styles.css'));
     return stream;
 });
@@ -19,3 +26,38 @@ gulp.task('minify-css', () => {
   });
   
 gulp.task('styles', gulp.series('sass', 'minify-css'));
+
+//combine then minify js files
+
+gulp.task('scripts', function() {
+    return gulp.src('./*.js')
+      .pipe(concat('bundle.js'))
+      .pipe(gulp.dest('./dist/'));
+  });
+
+gulp.task("uglify", function () {
+    return gulp.src("lib/bundle.js")
+        .pipe(rename("bundle.min.js"))
+        .pipe(uglify(/* options */))
+        .pipe(gulp.dest("lib/"));
+});
+
+
+// watch & default tasks
+
+gulp.task('watch', function () {
+	return gulp.watch('./scss/*.scss', gulp.series('styles'));
+});
+
+// Static Server + watching scss/html files
+gulp.task('serve', ['sass'], function() {
+
+    browserSync.init({
+        server: "./"
+    });
+
+    gulp.watch("./scss/*.scss", ['sass']);
+    gulp.watch("./*.html").on('change', browserSync.reload);
+});
+
+gulp.task('default', ['styles', 'serve']);
