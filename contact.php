41245c00cf5eaa82b133b491809be651dba520bd
Audrey Hayes <audreyhayes0@gmail.com>
2019-04-18 14:32:06 +0100

    added contact page scaffolding

---
+++ contact.php
@@ -0,0 +1,19 @@
+<?php
+    require_once('partials/header-top.php');
+    require_once('partials/navbar.php');
+?>
+
+<header class="justify-content-center">
+    <div class="lead">
+        <img class="d-block img-fluid"
+        src="dist/assets/imgs/unsplash.jpg">
+        <div class="subpage-title">
+            <h1>Contact</h1>
+        </div>
+    </div>
+</header>
+
+
+
+<?php
+include('partials/footer.php');
